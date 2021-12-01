b05223aca2389772dea9397428ccaa1af7a028d0
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-11-30 21:55:44 -0300

    código finalizado com sucesso

--- atividadeJavaScript/js/Atividade.js
+++ atividadeJavaScript/js/Atividade.js
@@ -1,8 +1,9 @@
+//tamanho font
 function tFontT(){
     let font = document.getElementById("tFont").value;
-    document.getElementById("paragrafo").style.fontSize = `${font}px`;
+    document.getElementById("tDiv").style.fontSize = `${font}px`;
 }
-
+//cor fonte
 function vermelho(){
     document.getElementById("paragrafo").style.color = "red";
 }
@@ -11,14 +12,32 @@ function verde(){
     document.getElementById("paragrafo").style.color = "green";
 }
 
+function cinza(){
+    document.getElementById("paragrafo").style.color = "silver";
+}
+
+function laranja(){
+    document.getElementById("paragrafo").style.color = "orange";
+}
+
+//cor background
 function preto(){
-    document.getElementById("paragrafo").style.background = "black";
+    document.getElementById("tDiv").style.background = "black";
 }
 
 function amarelo(){
-    document.getElementById("paragrafo").style.background = "yellow";
+    document.getElementById("tDiv").style.background = "yellow";
 }
 
+function verdeMarinho(){
+    document.getElementById("tDiv").style.background = "aqua";
+}
+
+function azul(){
+    document.getElementById("tDiv").style.background = "blue";
+}
+
+//tamanho Div
 function tDivT(){
     let divWidth = document.getElementById("tDivTexto").value;
     document.getElementById("tDiv").style.width = `${divWidth}`;
