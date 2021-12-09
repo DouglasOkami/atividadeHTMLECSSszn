ad3e4b6c30c3d0783cd02ef54b541a4d6a3f9397
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-12-09 20:49:15 -0300

    project finalizado

--- food-website/JS/script.js
+++ food-website/JS/script.js
@@ -7,12 +7,33 @@ menu.onclick = () => {
 }
 
 let section = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
-}
+
+  section.forEach(sec =>{
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    console.log(sec);
+
+    if(top >= offset && top < offset + height){
+      console.log("if");
+      navLinks.forEach(links => {
+        console.log(links);
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+
+    }//fim if
+  
+  });//fim section
+
+}//fim onscroll
 
 document.querySelector('#search-icon').onclick = () => {
   document.querySelector('#search-form').classList.toggle('active')
@@ -59,3 +80,13 @@ var swiper = new Swiper  (".review-slider", {
     },
   }, 
 });  
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
