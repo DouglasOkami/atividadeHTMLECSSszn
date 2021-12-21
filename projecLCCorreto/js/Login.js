aba65134f719379ea5eb2ab893bc3d26145bed16
Douglas da Silva Ferreira <meliodasbrpjt1@gmail.com>
2021-12-21 09:11:21 -0300

    código quase finallizado

--- projecLCCorreto/js/Login.js
+++ projecLCCorreto/js/Login.js
@@ -8,3 +8,53 @@ btn.addEventListener('click', ()=>{
         inputSenha.setAttribute('type', 'password')
     }
 });
+
+function entrar(){
+    let usuario = document.querySelector('#usuario');
+    let userLabel =  document.querySelector('#userLabel');
+
+    let senha = document.querySelector('#senha');
+    let senhaLabel =  document.querySelector('#senhaLabel');
+
+    let msgError =  document.querySelector('#msgError');
+    let listaUser = [];
+
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'));
+    listaUser.forEach(item => {
+        if(usuario.value == item.userCad && senha.value == item.senhaCad){
+          
+            userValid = {
+                nome: item.nomeCad,
+                user: item.userCad,
+                senha: item.senhaCad
+            }
+        }
+    });
+
+    console.log(usuario, userValid);
+
+    if(usuario.value == userValid.user && senha.value == userValid.senha){
+        let token = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2);
+        localStorage.setItem('token',token)
+        window.location.href = "http://127.0.0.1:5500/Inicio.html";
+        localStorage.setItem('userLogado', JSON.stringify(userValid));
+    }else{
+        userLabel.setAttribute('style', 'color:red');
+        usuario.setAttribute('style', 'border-color:red');
+        senhaLabel.setAttribute('style', 'color:red');
+        senha.setAttribute('style', 'border-color:red');
+        msgError.setAttribute('style', 'display: block');
+        msgError.innerHTML = ('Usuário ou senha incorretos');
+        usuario.focus()
+    }
+
+
+
+
+}
